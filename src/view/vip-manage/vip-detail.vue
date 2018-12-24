72349771eb9f0c2755eae0c055fb577beceb02e7
wyh <wooyoohoo@163.com>
2018-12-24 13:56:17 +0800

    完成会员详情页

--- src/view/vip-manage/vip-detail.vue
+++ src/view/vip-manage/vip-detail.vue
@@ -12,7 +12,7 @@
             </FormItem>
             <FormItem label="积分">
                 <Input disabled class="input special_input" v-model="vipFrom.vipScore"/>
-                <Button type="info" style="margin-left:5px;">积分调整</Button>
+                <Button type="info" style="margin-left:5px;" @click="adjustScore">积分调整</Button>
             </FormItem>
             <FormItem label="注册时间">
                 <Input disabled class="input" v-model="vipFrom.vipRegisterTime"/>
@@ -21,22 +21,23 @@
                 <Input class="input" type="textarea" v-model="vipFrom.vipRemark"/>
             </FormItem>
             <FormItem label="会员推荐人">
-                <Input class="input" disabled v-model="vipFrom.vipReferrer"/>
+                <Input class="input special_input" disabled v-model="vipFrom.vipReferrer"/>
+                <Button type="info" style="margin-left:5px;" @click="adjustReferrer">修改</Button>
             </FormItem>
             <FormItem label="是否禁用">
-                <RadioGroup v-model="isDisabled">
+                <RadioGroup v-model="vipFrom.isDisabled">
                     <Radio label="是"></Radio>
                     <Radio label="否" style="margin-left:5px;"></Radio>
                 </RadioGroup>
             </FormItem>
             <FormItem label="会员标签">
-                <CheckboxGroup v-model="vipLabel">
+                <CheckboxGroup v-model="vipFrom.vipLabel">
                     <Checkbox label="普通会员"></Checkbox>
                     <Checkbox label="忠实会员"></Checkbox>
                     <Checkbox label="内部会员"></Checkbox>
                 </CheckboxGroup>
             </FormItem>
-            <Button type="info" style="width:80px;">提交</Button>
+            <Button type="info" style="width:80px;" @click="submitFrom">提交</Button>
         </Form>
     </div>
 </template>
@@ -49,18 +50,123 @@ export default {
            vipAccount: "SVIP201834534654565",
            vipNickname: "王先生",
            vipPhone: "15136245589",
-           vipScore: "6666",
+           vipScore: 6666,
+           scoreRemark: "积分备注信息",
            vipRegisterTime: "2018-12-21",
            vipRemark: "备注信息",
-           vipReferrer: "推荐人名称/45465798798"
+           vipReferrer:"王先生/15136245578",
+           isDisabled: "",
+           vipLabel:[]
        },
-       // 是否禁用会员
-       isDisabled: "",
-       // 选中的vip标签
-       vipLabel: []
+       // 调整后的积分值
+       modifyScore: 0,
+       // 积分备注
+       modifyRemark: "",
+       // 新推荐人手机号
+       referrerPhone: ""
     };
   },
-  methods: {}
+  methods: {
+      /**
+       * 弹出积分调整弹框
+       */
+       adjustScore: function() {
+           this.$Modal.confirm({
+                onOk:this.submitModifyScore,
+                render: (h,params) => {
+                    return h("div",[
+                        h("InputNumber",{
+                            props: {
+                                placeholder: '积分值',
+                                value: parseInt(this.vipFrom.vipScore)
+                            },
+                            style: {
+                                width: '90px'
+                            },
+                            on: {
+                                'on-change': (number) => {
+                                    // 更改积分
+                                    this.modifyScore = number
+                                }
+                            }
+                        }),
+                        h("Input",{
+                            props: {
+                                placeholder: '注意：输入的数值即为最终积分值',
+                                disabled: true
+                            },
+                            style: {
+                                width: '260px',
+                                marginLeft: '25px'
+                            }
+                        }),
+                        h("Input",{
+                            props:{
+                                type: 'textarea',
+                                placeholder: "备注",
+                                value: this.vipFrom.scoreRemark
+                            },
+                            style: {
+                                marginTop: '20px'
+                            },
+                            on: {
+                                input: (info) => {
+                                    // 添加积分备注
+                                    this.modifyRemark = info
+                                }
+                            }
+                        })
+                    ])
+                }
+            })
+       },
+
+       /**
+        * 提交修改的积分值
+        */
+       submitModifyScore: function () {
+          this.vipFrom.vipScore = this.modifyScore
+          this.vipFrom.scoreRemark = this.modifyRemark
+       },
+
+       /**
+        * 修改推荐人信息
+        */
+       adjustReferrer: function() {
+           this.$Modal.confirm({
+                onOk:this.submitModifyReferrer,
+                render: (h,params) => {
+                    return h("div",[
+                        h('Input',{
+                            props:{
+                                placeholder: '输入新推荐人手机号'
+                            },
+                            on: {
+                                input: (newphone) => {
+                                    this.referrerPhone = newphone
+                                }
+                            }
+                        })
+                    ])
+                }
+            })
+       },
+
+       /**
+        * 提交修改的推荐人
+        */
+       submitModifyReferrer: function () {
+          // 修改推荐人信息
+          this.vipFrom.vipReferrer = this.referrerPhone
+       },
+
+       /**
+        * 提交表单
+        */
+       submitFrom: function() {
+           console.log(this.vipFrom)
+       }
+  }
 };
 </script>
 <style>

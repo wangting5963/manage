883fe7466d9fee5c6d23348edd260702f72cc276
838863902 <838863902@qq.com>
2019-01-16 13:34:59 +0800

    update code

--- src/view/order-manage/order-manage.vue
+++ src/view/order-manage/order-manage.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <button @click="getOrderInfo">获取订单详情</button>
-        <!-- 数据绑定 -->
-        <input v-model="value">  
-        <!-- 获取绑定的值 -->
-        <div>{{ value }}</div>
+        <Input v-model="value" placeholder="订单编号" class="order_head_input first" clearable/>
+        <Input v-model="value" placeholder="订单编号" class="order_head_input" clearable/>
+        <Input v-model="value" placeholder="订单编号" class="order_head_input" clearable/>
+        <Input v-model="value" placeholder="订单编号" class="order_head_input" clearable/>   
+        <button @click="getOrderInfo">获取订单信息</button>
     </div>
 </template>
 
@@ -14,12 +14,18 @@ export default {
     name: 'order-manage',
     data() {
         return {
-            value:""
+            orderNo:""
         }
     },
     methods:{
+        /**
+         * 获取所有订单信息
+         */
         getOrderInfo:function(){
-            request("mapi/order/selective.do","post",{"page":"1","pageSize":"10"},function(res){
+            let formData = new FormData()
+            formData.append("page","1")
+            formData.append("pageSize","10")
+            request("mapi/order/selective.do","post",formData,function(res){
                 console.log(res)
             })
         }
@@ -28,6 +34,13 @@ export default {
 </script>
 
 <style>
+    .order_head_input{
+        width: 200px;
+        margin-left: 20px;
+    }
 
+    .order_head_input.first{
+        margin-left: 0px;
+    }
 </style>
 
